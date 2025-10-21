beede6f81c3aba9af95b41cf0e9224a364ee6542
Halalisani <mbanjwa.hg@gmail.com>
2025-10-21 23:05:10 +0200

    Wrap app in gesture handler root view

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import CountryScreen from './screens/CountryScreen';
 import ProvinceScreen from './screens/ProvinceScreen';
@@ -17,25 +18,27 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <PostsProvider>
-      <SettingsProvider>
-        <SafeAreaProvider>
-          <NavigationContainer>
-            <Stack.Navigator
-              initialRouteName="Country"
-              screenOptions={{ headerShown: false }}
-            >
-              <Stack.Screen name="Country" component={CountryScreen} />
-              <Stack.Screen name="Province" component={ProvinceScreen} />
-              <Stack.Screen name="City" component={CityScreen} />
-              <Stack.Screen name="Room" component={RoomScreen} />
-              <Stack.Screen name="PostThread" component={PostThreadScreen} />
-              <Stack.Screen name="MyComments" component={MyCommentsScreen} />
-              <Stack.Screen name="Settings" component={SettingsScreen} />
-            </Stack.Navigator>
-          </NavigationContainer>
-        </SafeAreaProvider>
-      </SettingsProvider>
-    </PostsProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <PostsProvider>
+        <SettingsProvider>
+          <SafeAreaProvider>
+            <NavigationContainer>
+              <Stack.Navigator
+                initialRouteName="Country"
+                screenOptions={{ headerShown: false }}
+              >
+                <Stack.Screen name="Country" component={CountryScreen} />
+                <Stack.Screen name="Province" component={ProvinceScreen} />
+                <Stack.Screen name="City" component={CityScreen} />
+                <Stack.Screen name="Room" component={RoomScreen} />
+                <Stack.Screen name="PostThread" component={PostThreadScreen} />
+                <Stack.Screen name="MyComments" component={MyCommentsScreen} />
+                <Stack.Screen name="Settings" component={SettingsScreen} />
+              </Stack.Navigator>
+            </NavigationContainer>
+          </SafeAreaProvider>
+        </SettingsProvider>
+      </PostsProvider>
+    </GestureHandlerRootView>
   );
 }
